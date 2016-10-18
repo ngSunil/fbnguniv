1245fbeba97dd6e4c78b2d213f666a0e51e7ab20
Sunil <sun.aec@gmail.com>
2016-10-18 21:12:21 +0530

    Get lessons Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 import { AngularFireModule } from 'angularfire2/index';
 import { AppComponent } from './app.component';
 import { firebaseConfig } from '../environments/firebase.config';
 import { FirebaseComponent } from './firebase/firebase.component';
+import { HomeComponent } from './home/home.component';
+import { LessonsService } from './shared/model/lessons.service';
 @NgModule({
   declarations: [
     AppComponent,
-    FirebaseComponent
+    FirebaseComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule, AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [LessonsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/model/lessons.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Lesson } from './lesson';
+import { AngularFire } from 'angularfire2';
+@Injectable()
+export class LessonsService {
+
+  constructor(private af: AngularFire) {}
+  findallLessons(): Observable<Lesson[]>{
+    return this.af.database.list('lessons');
+  }
+
+}
