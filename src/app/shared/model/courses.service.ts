abee0d2464c4128f35305b0360db2ba63e99862d
Sunil <sun.aec@gmail.com>
2016-10-20 23:55:15 +0530

    Viewing Courses list reiterating over the returned observables

--- src/app/shared/model/courses.service.ts
+++ src/app/shared/model/courses.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFire } from 'angularfire2';
+import { Observable } from 'rxjs/Rx';
+import { Course } from './course';
 @Injectable()
 export class CoursesService {
 
-  constructor() { }
-
+  constructor(private af: AngularFire) { }
+  findAllCourses(): Observable<Course[]>{
+    return this.af.database.list('courses').map(Course.fromJsonArray);
+  }
 }
